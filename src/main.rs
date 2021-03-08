ed7386416beaef7eeeec4481bc0d6522cf47337f
addy-go <addy.gabriel.voice@gmail.com>
2021-03-08 16:34:26 -0500

    print to command line random sequence of notes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
 fn main() {
-    println!("Hello, world!");
+    // array to store the names of the 12 notes in the chromatic scale
+    let mut chromatic: [&str; 12] = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"];
+
+    //create random number generator used for shuffling array of notes
+    let mut rng = thread_rng();
+
+    //shuffle chromatic array in place
+    chromatic.shuffle(&mut rng);
+
+    //print shuffled array to command line
+    println!("random note sequence: {:?}", chromatic);
+  
 }
+
+//TODO: add tests
